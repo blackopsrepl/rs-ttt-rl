f75607e0e8a872e1b3a3a14db6efb93805af3239
Vittorio Distefano <info@vdistefano.studio>
2025-04-23 07:46:55 +0200

    feat(lib.rs): Add CRand struct and implementation
    - Added`lib.rs` which contains the implementation of a simple pseudo-random number generator called `CRand`. The `CRand` struct has a state variable and supports two methods: `rand()` and `rand_float()`.

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+/*
+Simple pseudo-random generator, aims to be similar to the standard C rand() function
+
+The `rand()` method implements a linbear congruential generator (LCG) calculation and aims to be compatible
+with the standard C `rand()` function, using a linear congruential generator (LCG) calculation. 
+It updates the state by multiplying it with a constant multiplier, adding an increment 
+and discarding the lower 16 bits to produce a random number between 0 and 32767.
+
+The `rand_float()` method returns a float between 0 and 1 by dividing the result of `rand()` by 32767.
+*/
+use std::time::{SystemTime, UNIX_EPOCH};
+
+pub struct CRand {
+    state: u32,
+}
+
+impl CRand {
+    const MULTIPLIER: u32 = 1103515245;
+    const INCREMENT: u32 = 12345;
+    const RAND_MAX: u32 = 0x7fff;
+
+    // Initializes with current time as seed
+    pub fn new() -> Self {
+        let start = SystemTime::now();
+        let since_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        CRand {
+            state: since_epoch.as_secs() as u32,
+        }
+    }
+
+    // Implements the same algorithm as standard C rand()
+    pub fn rand(&mut self) -> u32 {
+        // runs LCG calculation
+        self.state = self
+            .state
+            .wrapping_mul(Self::MULTIPLIER)
+            .wrapping_add(Self::INCREMENT);
+        // discards lower 16 bits
+        (self.state >> 16) & Self::RAND_MAX
+    }
+
+    // Returns a float between 0 and 1
+    pub fn rand_float(&mut self) -> f32 {
+        self.rand() as f32 / Self::RAND_MAX as f32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_random_weight() {
+        let mut rng = CRand::new();
+        let weight = rng.rand_float() - 0.5;
+        assert!(
+            weight >= -0.5 && weight < 0.5,
+            "Weight should be in [-0.5, 0.5)"
+        );
+        print!("{}\n", weight)
+    }
+}
